d0a2e84bd4f4cb920d9c1554a5fc820cb667a8dd
Percyangel.J <angelpercy64@gmail.com>
2015-10-28 12:44:29 +0530

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
- before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
+  def create
     @user = User.create(user_params)
     if @user.save
       flash[:success]="Successfully created"
@@ -20,16 +20,16 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-	def show
+  def show
 
-	end
+  end
 
-	def edit
+  def edit
 
-	end
+  end
 
-	def update
-      @user = User.find(params[:id])
+  def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path
     else
@@ -43,19 +43,18 @@ class UsersController < ApplicationController
     redirect_to user_path
   end
 
-private
+  private
 
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    def user_params
-      params.require(:user).permit( :first_name, 
-                                    :last_name,  
-                                    :dob,
-                                    :city,
-                                    :address,
-                                    :gender)
-    end
+  def user_params
+    params.require(:user).permit( :first_name,
+                                  :last_name,
+                                  :dob,
+                                  :city,
+                                  :address,
+                                  :gender)
+  end
 end
- 
